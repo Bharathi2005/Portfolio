d8d376bcad15eebf4b9ddf97d2291cfcd206bcdf
BHARATHI A K <bharathiak2005@gmail.com>
2024-09-24 12:31:01 +0530

    change

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+const App=()=>{
+    return(
+        <>
+        <Navbar/>
+        <Home/>
+        </>
+    )
+}
+export default App
---
+++ portfolio/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+const Home=()=>{
+    return(
+    <>
+    <div className='font-semibold'>BHARATHI A K</div>
+    <div className='bg-slate-400'>
+        <img src='../assets/img/20220907_154445.jpg'/>
+    </div>
+    </>
+    )
+}
+export default Navbar
